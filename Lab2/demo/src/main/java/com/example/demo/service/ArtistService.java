c4bcff213660de2a346808adb6603f774733da95
LuuD04N <doanphongluu082@gmail.com>
2025-03-19 07:27:55 +0700

    Update ArtistService.java

--- Lab2/demo/src/main/java/com/example/demo/service/ArtistService.java
+++ Lab2/demo/src/main/java/com/example/demo/service/ArtistService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 import com.example.demo.model.Artist;
 import com.example.demo.repository.ArtistRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +12,6 @@ public class ArtistService {
     
     private final ArtistRepository artistRepository;
     
-    @Autowired
     public ArtistService(ArtistRepository artistRepository) {
         this.artistRepository = artistRepository;
     }
